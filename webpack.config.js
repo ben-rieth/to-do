e29e9c99eebca08dd96f03f71712ae1cca704c88
ben-rieth <benrieth3@gmail.com>
2022-05-19 15:30:07 -0500

    Setup webpack

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'To Do',
+            template: "./src/index.html"
+        })
+    ],
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist'
+    },
+    mode: 'development',
+    optimization: {
+        runtimeChunk: 'single'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    }
+}
