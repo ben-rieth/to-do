79eb9731f9166e0cbc36aca66b93dbb65bb391dc
ben-rieth <benrieth3@gmail.com>
2022-05-19 20:24:29 -0500

    Update Task to require all parameters

--- src/models/task.ts
+++ src/models/task.ts
@@ -1,19 +1,22 @@
 import { Priority } from './priority';
 
 class Task {
+    static NUM_TASK: number = 0;
+
     name: string;
     priority: Priority;
-    dueDate?: string;
-    dueTime?: string;
-    notes?: string;
+    dueDate: string;
+    dueTime: string;
+    notes: string;
     _dateAdded: string;
     _complete: boolean;
+    _id: number;
 
     constructor(name: string, 
                 priority: Priority,
-                dueDate?: string,
-                dueTime?: string,
-                notes?: string) {
+                dueDate: string,
+                dueTime: string,
+                notes: string) {
         this.name = name;
         this.priority = priority;
         this.dueDate = dueDate;
@@ -22,6 +25,8 @@ class Task {
 
         this._dateAdded = Date();
         this._complete = false;
+
+        this._id = Task.NUM_TASK++;
     }
 
     get dateAdded() : string {
