410c9cc2f4587212b318a17c0f9787807f51f0a6
ben-rieth <benrieth3@gmail.com>
2022-05-19 20:29:03 -0500

    Remove commented out code from Task class

--- src/models/task.ts
+++ src/models/task.ts
@@ -43,38 +43,4 @@ class Task {
 
 }
 
-// const Task = (name: string, 
-//             priority: "none" | "low" | "mid" | "high" = "none",
-//             dueDate?: string | "none",
-//             dueTime?: string | "none", 
-//             notes?: string) => {
-
-//     let dateAdded = Date();
-//     let complete = false;
-
-//     const getDateAdded = () : string => dateAdded;
-
-//     const completeTask = () => {
-//         complete = true;
-//     }
-
-//     const unCompleteTask = () => {
-//         complete = false;
-//     }
-
-//     const isComplete = () : boolean => complete;
-
-//     return {
-//         name,
-//         priority,
-//         dueDate,
-//         dueTime,
-//         notes,
-//         getDateAdded,
-//         completeTask,
-//         unCompleteTask,
-//         isComplete
-//     }
-// }
-
 export { Task };
